57d1a308e3a74e06fad720fe1fb4a97deb867447
Connor Sutton <connorsutton07@gmail.com>
2021-04-13 22:09:41 -0500

    added __init__

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+Modules:
+--------
+
+__main__.py
+    entrance/driver
+
+agent.py
+    defines and implements Q-Learning agent
+
+grid.py
+    provides environment to test Q-Learning agent
+    
+"""
